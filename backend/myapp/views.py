b00e9a26a85c6026f2ccb0d1e5c118f118458b7c
krebdoe <shashank.singh.2@mnsu.edu>
2026-04-28 12:32:29 -0500

    import isAuthenticated

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, generics, permissions, status, serializers
 from rest_framework.decorators import api_view, action, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.exceptions import AuthenticationFailed, TokenError
 from django.db.models import Q, Prefetch
